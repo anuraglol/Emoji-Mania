9baafeaef7386106b51622b981f06dae388be048
Anurag <77309809+kr-anurag@users.noreply.github.com>
2021-07-30 17:07:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import "./styles.css";
+import { useState } from "react";
+export default function App() {
+  var [emoji, setEmoji] = useState("");
+  async function fetchEmoji() {
+    const url = "https://emoji-api.com/emojis?search=";
+    const api = "&access_key=740e54afa31f4da2388c6e5f3f074815a61a4b17";
+    const BASEUrl = `${url}${emoji}${api}`;
+    const res = await fetch(BASEUrl);
+    const data = await res.json();
+    let i;
+    document.querySelector(".res").innerHTML = "";
+    try {
+      for (i = 0; i <= data.length; i++) {
+        var x = data[i].character;
+        var y = data[i].slug;
+        if ((x !== "") & (y !== "")) {
+          document.querySelector(".res").innerHTML += `<p>${x}<br />${y}</p>`;
+        }
+      }
+    } catch (err) {}
+  }
+  return (
+    <div className="App">
+      <input
+        type="text"
+        onChange={(e) => setEmoji(e.target.value)}
+        className="m-2 p-1 bg-white rounded shadow"
+      />
+      <button
+        onClick={fetchEmoji}
+        className="bg-green-600 rounded shadow p-2 hover:bg-green-400"
+      >
+        Search
+      </button>
+      <div className="res"></div>
+      <span>
+        Created with 💖 by{" "}
+        <a
+          href="https://kr-anurag.netlify.app/"
+          target="blank"
+          className="underline"
+        >
+          Anurag
+        </a>
+      </span>
+    </div>
+  );
+}
